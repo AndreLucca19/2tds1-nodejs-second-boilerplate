e97306806a60c639d2529c1b2b412520222f02dc
André Lucca <157771933+AndreLucca19@users.noreply.github.com>
2024-10-31 17:17:04 -0300

    feat: create update user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -44,7 +44,20 @@ usuariosRoutes.get("/:id", (req, res) => {
 });
 
 usuariosRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+    
+    const user = userList.updateUser(id, name, email, password);
 
+    if (!user) {
+        return res.status(404).json({
+            message: `Usuário com o id ${id} não encontrado!`,
+        });
+    }   
+        return res.status(200).json({
+            message: `Usuário com o id ${id} atualizado!`,
+            user,
+        });
 });
 
 usuariosRoutes.delete("/:id", (req, res) => {
